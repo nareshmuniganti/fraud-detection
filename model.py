3bea42778d9aefb47531fb4a8df3e37269df766a
nareshmuniganti <naresh.muniganti@riversand.com>
2021-10-28 00:59:11 -0400

    added 3 new ML models

--- model.py
+++ model.py
@@ -1,1 +1,13 @@
 print("modelling related code")
+
+def model1():
+    print("adding code for ML model1")
+
+def model2():
+    print("adding code for ML model2")
+
+def model3():
+    print("adding code for ML model3")
+
+def model4():
+    print("adding code for ML model4")
